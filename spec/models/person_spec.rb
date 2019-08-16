d0d0210e0bcab0e4ed710660fa5543bd2e172b6c
Laura Siriani <lausiriani@hotmail.com>
2019-08-15 22:37:31 -0300

    Feat: Add test to validate date formatting from model

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,4 +4,14 @@ describe Person, type: :model do
   context 'Associations' do
     it { should have_many(:animals) }
   end
+
+  context 'Creation' do
+    let(:dt_of_birth){ Faker::Date.birthday(min_age: 1, max_age: 65) }
+    let(:brazilian_dt_of_birth){ I18n.l(dt_of_birth, format: :long) }
+    let(:person){ build(:person) }
+    
+    it 'should have DateTime dt_birth before save' do
+      expect(person.convert_to_date(brazilian_dt_of_birth).class).to eq(DateTime)
+    end
+  end
 end
