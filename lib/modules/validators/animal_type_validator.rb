d9ff2945a5c00f900cb71720faea2c234ab78b3c
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 11:53:23 -0300

    Fix: Change restric_animal_type method to restric_animal_type on AnimalTypeValidator

--- lib/modules/validators/animal_type_validator.rb
+++ lib/modules/validators/animal_type_validator.rb
@@ -1,7 +1,7 @@
 module Validators
   module AnimalTypeValidator
 
-    def self.restric_animal_type
+    def self.restric_bird_animal_type
       animal_type = AnimalType.find_by(animal_type: 'Andorinha')
       animal_type
     end
