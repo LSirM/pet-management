9e800d7b72a517c2eb84aac4970f99f004fcfa9c
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 16:32:14 -0300

    Feat: Add dt_birth to person factory

--- spec/factories/person.rb
+++ spec/factories/person.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :person do
     name { Faker::Name.name }
     document  { Faker::IDNumber.brazilian_id }
+    dt_birth { I18n.l(Faker::Date.birthday(min_age: 1, max_age: 65), format: :long) }
   end
 end
