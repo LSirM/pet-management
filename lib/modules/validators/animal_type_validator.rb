b7a98d010f19d59e5c0493a5c999a4f1bf916957
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 11:53:48 -0300

    Feat: Add restrict_cat_animal_type validation to AnimalTYpeValidator

--- lib/modules/validators/animal_type_validator.rb
+++ lib/modules/validators/animal_type_validator.rb
@@ -5,5 +5,9 @@ module Validators
       animal_type = AnimalType.find_by(animal_type: 'Andorinha')
       animal_type
     end
+
+    def self.restrict_cat_animal_type
+      animal_type = AnimalType.find_by(animal_type: 'Gato')
+      animal_type
   end
 end
