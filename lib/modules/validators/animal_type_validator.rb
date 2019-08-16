aa140cf9b41223f3552f168b7defadb70a12848b
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 02:06:52 -0300

    Feat: Add AnimalTypeValidator module to return Andorinha animal_type

---
+++ lib/modules/validators/animal_type_validator.rb
@@ -0,0 +1,9 @@
+module Validators
+  module AnimalTypeValidator
+
+    def self.restric_animal_type
+      animal_type = AnimalType.find_by(animal_type: 'Andorinha')
+      animal_type
+    end
+  end
+end
