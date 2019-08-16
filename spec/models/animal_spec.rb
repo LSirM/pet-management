f23abc2785de6c8a409084b3e6b9417a919922bc
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 12:06:55 -0300

    Fix: Remove variables from context and place them inside describe block on AnimalTypeSpec

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -6,15 +6,15 @@ describe Animal, type: :model do
     it { should belong_to(:animal_type) }
   end
 
-  context 'AnimalType' do
-    let(:valid_person){ create(:person, dt_birth: 20.years.ago) }
-    let(:invalid_person){ create(:person, dt_birth: 8.years.ago) }
-    
-    let(:animal_type){ create(:animal_type, animal_type: 'Andorinha') }
-    let!(:first_animal){ build(:animal, person_id: valid_person.id, animal_type_id: animal_type.id) }
-    let!(:second_animal){ build(:animal, person_id: invalid_person.id, animal_type_id: animal_type.id) }
-    
+  context 'AnimalType' do    
     describe 'Andorinhas' do
+      let(:valid_person){ create(:person, dt_birth: 20.years.ago) }
+      let(:invalid_person){ create(:person, dt_birth: 8.years.ago) }
+      
+      let(:animal_type){ create(:animal_type, animal_type: 'Andorinha') }
+      let!(:first_animal){ build(:animal, person_id: valid_person.id, animal_type_id: animal_type.id) }
+      let!(:second_animal){ build(:animal, person_id: invalid_person.id, animal_type_id: animal_type.id) }
+
       it 'should succeed if Person is at least 18 years old' do
         first_animal.save
         expect(first_animal).to be_persisted
