c40e257d392f3547c4727ee4b696195a1b5ff5c5
Laura Siriani <lausiriani@hotmail.com>
2019-08-15 23:18:42 -0300

    Feat: Include DateFormatter to model and use it on convert_to_date method

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,9 @@
 class Person < ApplicationRecord
+  include Formatters::DateFormatter
+
   has_many :animals
+
+  def convert_to_date(date)
+    Formatters::DateFormatter.convert_brazilian_date_to_default(date)
+  end
 end
