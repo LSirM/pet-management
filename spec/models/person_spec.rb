aac75dee045d28200916770f9c537d2006673723
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 16:32:55 -0300

    Fix: Remove dt_birth assignment on test and use factor date

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -6,18 +6,14 @@ describe Person, type: :model do
   end
 
   context 'Creation' do
-    let(:dt_of_birth){ Faker::Date.birthday(min_age: 1, max_age: 65) }
-    let(:brazilian_dt_of_birth){ I18n.l(dt_of_birth, format: :long) }
     let(:person){ build(:person) }
-    
     it 'should have DateTime dt_birth before save' do
-      expect(person.convert_to_date(brazilian_dt_of_birth).class).to eq(DateTime)
+      expect(person.dt_birth.class).to eq(ActiveSupport::TimeWithZone)
     end
 
     it { should be_valid(person) }
 
     it 'should be persisted' do
-      person.convert_to_date(brazilian_dt_of_birth)
       person.save
       expect(person).to be_persisted
     end
@@ -25,10 +21,7 @@ describe Person, type: :model do
   end
 
   describe 'Age' do
-    let(:dt_of_birth){ Faker::Date.birthday(min_age: 1, max_age: 65) }
-    let(:brazilian_dt_of_birth){ I18n.l(dt_of_birth, format: :long) }
-    let(:person){ create(:person, dt_birth: Person.new.convert_to_date(brazilian_dt_of_birth)) }
-
+    let(:person){ build(:person)}
     let(:age){ (Date.today.year - person.dt_birth.year) }
 
     it "should return person's age" do
