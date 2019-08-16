f3fca2f18911eb4619cd80b3ae9eea6364e4c9c6
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 12:48:23 -0300

    Fix: Remove validates_animal_type method from Animal model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -7,11 +7,6 @@ class Animal < ApplicationRecord
 
   private
 
-    def validates_animal_type
-      return true if validate_by_person_age
-      throw(:abort)
-    end
-
     def bird_limitation
       Validators::AnimalTypeValidator.restric_bird_animal_type
     end
