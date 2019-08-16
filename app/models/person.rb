da312a3dee718c1de5c0cabeca9064207b64ecfb
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 02:05:32 -0300

    Feat: Add age method to person

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,4 +6,8 @@ class Person < ApplicationRecord
   def convert_to_date(date)
     Formatters::DateFormatter.convert_brazilian_date_to_default(date)
   end
+
+  def age
+    (Date.today.year - self.dt_birth.year)
+  end
 end
