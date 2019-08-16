5fbbc6a3412246d14da52107f7f96d4610498479
Laura Siriani <lausiriani@hotmail.com>
2019-08-15 21:55:21 -0300

    Fix: Remove dt_birth from person factory

--- spec/factories/person.rb
+++ spec/factories/person.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
   factory :person do
     name { Faker::Name.name }
     document  { Faker::IDNumber.brazilian_id }
-    dt_birth { Faker::Date.birthday(min_age: 1, max_age: 65) }
   end
 end
