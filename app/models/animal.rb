56bde1b36c7a6501bcaf0d9917b56b6108c53995
Laura Siriani <lausiriani@hotmail.com>
2019-08-16 02:38:56 -0300

    Feat: Add animal before_save validation to verify person age-animal_type and add test

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,18 @@
 class Animal < ApplicationRecord
+  include Validators::AnimalTypeValidator
   belongs_to :person
   belongs_to :animal_type
+
+  before_save :validates_animal_type
+
+  private
+
+    def validates_animal_type
+      return true if !(self.animal_type_id == animal_type_limitation&.id && self.person.age < 18)
+      throw(:abort)
+    end
+
+    def animal_type_limitation
+      Validators::AnimalTypeValidator.restric_animal_type
+    end
 end
